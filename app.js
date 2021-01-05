c248f09eb175bac77a01266d8cb604e04bf9413f
Andreza Campbell <campbell.andreza@gmail.com>
2021-01-04 20:06:58 -0500

    parsing data from webpage to server, rendering on the screen using ejs, creating a list

--- app.js
+++ app.js
@@ -1,48 +1,34 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+let items = ["Buy food", "Cook Food", "Eat Food"];
+
+// let items = ["Buy Food", "Cook Food", "Eat Food"];
 
 const app = express();
 app.set("view engine", "ejs");
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/", function(req, res) {
     let today = new Date();
-    let currentDay = today.getDay();
-    let day = "";
+    let options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
 
-    switch (currentDay) {
-        case 0:
-            day = "Sunday!"
-            break;
-        case 1:
-            day = "Monday!"
-            break;
-        case 2:
-            day = "Tuesday!"
-            break;
-        case 3:
-            day = "Wednesday!"
-            break;
-        case 4:
-            day = "Thursday!"
-            break;
-        case 5:
-            day = "Friday!"
-            break;
-        case 6:
-            day = "Saturday!"
-            break;
-        default:
-            console.log("Something went wrong, check the swich case " + currentDay);
-            break;
-    }
+    let day = today.toLocaleDateString("en-US", options);
 
-    // if (currentDay === 6 || currentDay === 0) {
-    //     day = "weekend";
-    // } else {
-    //     day = "weekday";
-    // }
+    res.render("index", { kindOfDay: day, newListItem: items });
+});
 
-    res.render("index", { kindOfDay: day });
+app.post("/", function(req, res) {
+    item = req.body.newItem;
+
+    items.push(item);
+
+    res.redirect("/");
+
 });
 
 app.listen(3000, function() {
