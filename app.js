d8f54314d866759fbd7ff89ba3e0df0f397f4b06
Andreza Campbell <campbell.andreza@gmail.com>
2021-01-08 10:51:31 -0500

    adding date.js, refactoring code, css still not working

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-let items = ["Buy food", "Cook Food", "Eat Food"];
+const date = require(__dirname + "date.js");
+
+const items = ["Buy food", "Cook Food", "Eat Food"];
+const workItems = [];
 
 const app = express();
 app.set("view engine", "ejs");
@@ -9,20 +12,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", function(req, res) {
-    let today = new Date();
-    let options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    };
 
-    let day = today.toLocaleDateString("en-US", options);
+    const day = date.getDate();
 
     res.render("index", { kindOfDay: day, newListItem: items });
 });
 
 app.post("/", function(req, res) {
-    item = req.body.newItem;
+    const item = req.body.newItem;
 
     items.push(item);
 
